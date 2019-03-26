f27d35e62646522857ca4499909b3ba298181add
Gloria Chen <gloriachen@192-168-1-8.tpgi.com.au>
2019-03-27 10:23:03 +1100

    constructing-promise.js

---
+++ consturcting-promises-object.js
@@ -0,0 +1,15 @@
+const myExecutor = (resolve, reject) => {
+    if (inventory.sunglasses > 0) {
+        resolve('Sunglasses order processed.');
+    } else {
+        reject('That item is sold out.');
+    }
+};
+
+const orderSunglasses = () => {
+    return new Promise(myExecutor);
+};
+
+const orderPromise = orderSunglasses();
+
+console.log(orderPromise);                      
