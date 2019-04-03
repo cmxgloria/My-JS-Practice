aa043661c2585461a1bc9c5765fdabf278f8a012
Gloria Chen <gloriachen@192-168-1-7.tpgi.com.au>
2019-04-03 13:12:00 +1100

    xhr-post-requestII.js

---
+++ xhr-post-requestII.js
@@ -0,0 +1,14 @@
+const xhr = new XMLHttpRequest();
+const url = 'https://api-to-call.com/endpoint';
+const data = JSON.stringify({id: '200'});
+
+xhr.responseType = 'json';
+
+xhr.onreadystatechange = () => {
+  if(xhr.readyState === XMLHttpRequest.DONE){
+    return xhr.response;
+  }
+}
+
+xhr.open('POST', url);
+xhr.send(data);
