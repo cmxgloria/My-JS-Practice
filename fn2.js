9857d722c5644a3ecedc308eef0160319819dd7a
Gloria Chen <cmxgloria@gmail.com>
2019-07-27 13:07:21 +1000

    hoisting

--- fn2.js
+++ fn2.js
@@ -2,3 +2,15 @@ function printId(orderId) {
   console.log('printing id is ' + orderId);
 };
 printId(9002);
+
+
+
+
+
+// printId(9002);
+// function printId(orderId) {
+//   console.log('printing id is ' + orderId);
+// };
+// // same output
+
+
