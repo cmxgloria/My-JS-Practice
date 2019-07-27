897ffb0fe488608ece8f0adef6cea7a455f7d84c
Gloria Chen <cmxgloria@gmail.com>
2019-07-27 13:32:03 +1000

    2 consoles

---
+++ fn4.js
@@ -0,0 +1,8 @@
+function calTotalPrice(quatity, price) {
+  return quatity * price;
+};
+var totalPrice = calTotalPrice(3, 4);
+console.log(totalPrice);
+
+totalPrice = calTotalPrice(2, 8);
+console.log(totalPrice);
