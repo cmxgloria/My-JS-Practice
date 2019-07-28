4e95a5b6a465c509f533c9303d5bd412fab70117
Gloria Chen <cmxgloria@gmail.com>
2019-07-28 14:31:23 +1000

    global scope

---
+++ globalScope2.js
@@ -0,0 +1,8 @@
+var description = 'original product';
+function updateProduct() {
+  var updateProductId = function () {
+    console.log(description);
+  }
+  updateProductId();
+}
+updateProduct();
