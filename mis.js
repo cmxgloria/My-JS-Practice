de86b2c678470a26966d7233ac0746896cb36a47
Gloria Chen <cmxgloria@gmail.com>
2019-07-30 14:54:47 +1000

    misc js

---
+++ mis.js
@@ -0,0 +1,16 @@
+var total = 8;
+total *= 4 + 1;
+console.log(total);
+//40 , () precedence occur first
+
+var total = 99, tax = 9;
+console.log(total);
+console.log(tax);
+//99
+//9
+
+var total = (99, 87, 33);
+console.log(total);
+//33, only take the last one, dont suggest to use it.
+
+
