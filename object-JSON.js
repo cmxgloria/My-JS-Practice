d6b7e387225909a72ab06410d2165cffad82d771
Gloria Chen <cmxgloria@gmail.com>
2019-07-31 19:57:29 +1000

    updated object

--- object-JSON.js
+++ object-JSON.js
@@ -5,4 +5,17 @@ project.updateDueDate = function () {
   return true;
 };
 console.log(project['securityLevel']);
-//15, either dot notation or [' '] with string inside
+//15, either dot notation or [' '] with object as a string
+
+
+var project = new Object();
+project.name = 'Rocking plan';
+project.securityLevel = 15;
+project.updateDueDate = function () {
+  return true;
+};
+var field = 'securityLevel';
+console.log(project[field]);
+//15. we use variable and string to specify the property name, by using bracket notation
+
+
