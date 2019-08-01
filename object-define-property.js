2535b6c3ad27885e0b3facfa9134aa4a52ff238b
Gloria Chen <cmxgloria@gmail.com>
2019-08-01 21:44:12 +1000

    enumerable

--- object-define-property.js
+++ object-define-property.js
@@ -15,3 +15,22 @@ task.text = task.text + ' ... now';
 console.log(task.text);
 //'Get this job done ... now'
 
+'use strict'
+var task = {};
+Object.defineProperty(task, 'text', {
+  value: 'Get this job done',
+  // writable: true
+});
+task.text = task.text + ' ... now';
+console.log(task.text);
+//error: Uncaught TypeError: Cannot assign to read only property 'text' of object '#<Object>'
+
+//'use strict'
+var task = {};
+Object.defineProperty(task, 'text', {
+  value: 'Get this job done',
+  enumerable: true
+});
+for (var f in task)
+  console.log(f);
+//
