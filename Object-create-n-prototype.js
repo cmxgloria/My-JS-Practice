86d0172a0d263e5fe17cf75ab11f9558ea7a2d72
Gloria Chen <cmxgloria@gmail.com>
2019-08-01 21:26:25 +1000

    prototype

--- Object-create-n-prototype.js
+++ Object-create-n-prototype.js
@@ -12,3 +12,12 @@ var securityProject = Object.create(project);
 console.log(typeof securityProject.toString);
 //'function'
 
+
+var project = {
+  securityLevel: 2
+};
+var securityProject = Object.create(project);
+console.log(securityProject.__proto__.__proto__ === Object.prototype);
+//true
+
+
