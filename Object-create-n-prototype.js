c3befc2f14bbe3345809d4fadb4b4e0bb8000174
Gloria Chen <cmxgloria@gmail.com>
2019-08-01 21:20:14 +1000

    typeof

--- Object-create-n-prototype.js
+++ Object-create-n-prototype.js
@@ -4,3 +4,11 @@ var project = {
 var securityProject = Object.create(project);
 console.log(securityProject.securityLevel);
 //2
+
+var project = {
+  securityLevel: 2
+};
+var securityProject = Object.create(project);
+console.log(typeof securityProject.toString);
+//'function'
+
