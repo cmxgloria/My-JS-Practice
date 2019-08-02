5f4e57a3cdde3b81692e4d7d5a60bfd254707610
Gloria Chen <cmxgloria@gmail.com>
2019-08-02 11:00:47 +1000

    Object prototype

--- miscObject.js
+++ miscObject.js
@@ -22,5 +22,56 @@ var project = {
 console.log(project.__proto__.hasOwnProperty('toString'));
 //true
 
+'use strict'
+var project = {
+  name: 'earth reserch',
+  dueDate: '4/3/2019'
+}
+console.log(Object.prototype.isPrototypeOf(project));
+//true
 
+
+var project = {
+  securityLevel: 2
+}
+var securityProject = Object.create(project);
+console.log(project.isPrototypeOf(securityProject));
+//true
+
+
+var project = {
+  securityLevel: 2
+}
+var securityProject = Object.create(project);
+console.log(Object.prototype.isPrototypeOf(securityProject));
+//true
+
+var project = {
+  securityLevel: 2
+}
+var securityProject = Object.create(project);
+console.log(project.__proto__.isPrototypeOf(securityProject));
+//true
+
+
+var project = {
+  securityLevel: 2
+}
+var securityProject = Object.create(project);
+console.log('securityLevel' in securityProject);
+//true
+
+var project = {
+  securityLevel: 2
+}
+var securityProject = Object.create(project);
+console.log('toString' in securityProject);
+//true
+
+var project = {
+  securityLevel: 2
+}
+var securityProject = Object.create(project);
+console.log('securityLevel' in Object.prototype);
+  //false
 
