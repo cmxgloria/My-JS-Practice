4c0e106ba66317429f9f53d53da04176ff509d47
Gloria Chen <cmxgloria@gmail.com>
2019-08-02 11:16:42 +1000

    updated name expression

--- function.js
+++ function.js
@@ -1,3 +1,14 @@
 //function expressions, constructor function, 'this' keyword, calling function(call and apply),closure,IIFE's
+var hireEmployee = function (name) {
+
+};
+console.log(typeof hireEmployee);
+//function
+
+var hireEmployee = function (name) {
+  throw ('error');
+};
+hireEmployee('JJ');
+//error: Uncaught error
 
 
