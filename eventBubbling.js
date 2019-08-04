5946c9861be5fbbe0707b6b3345b376e2fa5579a
Gloria Chen <cmxgloria@gmail.com>
2019-08-04 13:23:25 +1000

    updated event bubbling

--- eventBubbling.js
+++ eventBubbling.js
@@ -46,3 +46,23 @@ var clickHandler = function divClickHandler(event) {
 div1.addEventListener('click', clickHandler, true);
 div2.addEventListener('click', clickHandler, true);
 //div1
+
+//body of HTML
+<div id='div1' style="width:200px; height:200px; background-color:red">
+  <div id='div2' style="width:200px; height:200px; background-color:red" ></div>
+</div>
+
+//Javascript file
+var div1 = document.getElementById('div1');
+var div2 = document.getElementById('div2');
+var clickHandler = function divClickHandler(event) {
+  console.log(this.id);
+  event.stopPropagation();
+  event.preventDefault();
+};
+div1.addEventListener('click', clickHandler, true);
+div2.addEventListener('click', clickHandler, true);
+//div1
+
+
+
