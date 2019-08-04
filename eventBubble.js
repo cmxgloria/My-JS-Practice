662d72ae530db83b45da11e29564331027fea9e4
Gloria Chen <cmxgloria@gmail.com>
2019-08-04 13:15:47 +1000

    event bubbling

---
+++ eventBubble.js
@@ -0,0 +1,16 @@
+//body of HTML
+<div id='div1' style="width:200px; height:200px; background-color:red">
+  <div id='div2' style="width:200px; height:200px; background-color:red" ></div>
+</div>
+
+//Javascript file
+var div1 = document.getElementById('div1');
+var div2 = document.getElementById('div2');
+var clickHandler = function divClickHandler(event) {
+  console.log(this.id);
+};
+div1.addEventListener('click', clickHandler, false);
+div2.addEventListener('click', clickHandler, false);
+//div2
+//div1
+
