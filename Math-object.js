789a2f52ad2b1ca6938f3b5581ea3feba65c7f96
Gloria Chen <cmxgloria@gmail.com>
2019-08-04 14:56:27 +1000

    updated Math Object

---
+++ Math-object.js
@@ -0,0 +1,49 @@
+var value = Math.PI;
+console.log(value);
+//3.141592653589793
+
+var value = Math.abs(-42);
+console.log(value);
+//42
+
+var value = Math.ceil(11.1);
+console.log(value);
+//12
+
+var value = Math.floor(21.1);
+console.log(value);
+//21
+
+var value = Math.trunc(42.1);
+console.log(value);
+//42
+
+var value = Math.max(-12, 0, 23, '88');
+console.log(value);
+//88, convert string to number
+
+var value = Math.min(-12, 0, 23, '88');
+console.log(value);
+//-12
+
+var value = Math.pow(2, 4);
+console.log(value);
+//16
+
+
+var value = Math.random();
+console.log(value);
+//0.56748, the number between 0-1
+
+var value = Math.sqrt(81);
+console.log(value);
+//9
+
+var value = Math.sqrt(-81);
+console.log(value);
+//NaN
+
+
+
+
+
