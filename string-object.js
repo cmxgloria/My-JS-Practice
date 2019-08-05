1fe1e5b38056ce41fe3ca82eb7ec51161ff51b80
Gloria Chen <cmxgloria@gmail.com>
2019-08-05 19:32:13 +1000

    updated

--- string-object.js
+++ string-object.js
@@ -15,3 +15,19 @@ var value = 'My String';
 console.log(value.endsWith('ing'));
 //true
 
+
+var value = 'My String';
+console.log(value.indexOf('M'));
+//0, if you couldnt find it, it will output -1, such as indexOf('z'), it turns -1
+
+
+var value = 'Some String';
+console.log(value.lastIndexOf('S'));
+//5
+
+var value = 'Some String';
+console.log(value.slice(5));
+//String
+
+
+
