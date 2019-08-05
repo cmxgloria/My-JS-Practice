2326ca8195dfbb5c0668c5cc4aea876bcfe3c4d3
Gloria Chen <cmxgloria@gmail.com>
2019-08-05 19:23:12 +1000

    concat includes endsWith

--- string-object.js
+++ string-object.js
@@ -2,3 +2,11 @@ var value = 'My String';
 console.log(value.charAt(3));
 //S
 
+var value = 'My String';
+console.log(value.concat(' Lives'));
+//'My String Lives'
+
+var value = 'My String';
+console.log(value.includes(' '));
+//true
+
