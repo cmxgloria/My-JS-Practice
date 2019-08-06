248c337be2e19097490b2c677fb1b282b1bec254
Gloria Chen <cmxgloria@gmail.com>
2019-08-06 11:58:27 +1000

    updated error handler

--- Misc.js
+++ Misc.js
@@ -6,3 +6,35 @@ catch (e) {
 }
 //'Error - Customer Error'
 
+try {
+  throw new Error('Customer Error');
+}
+catch (e) {
+  console.log(e.name + ' - ' + e.message);
+}
+finally {
+  console.log('Finally Done');
+}
+//'Error - Customer Error'
+//'Finally Done'
+
+try {
+  var i = 42;
+}
+catch (e) {
+  console.log(e.name + ' - ' + e.message);
+}
+finally {
+  console.log('Finally Done');
+}
+// 'Finally Done'
+
+try {
+  throw new RangeError('Range Error');
+}
+catch (e) {
+  if (e instanceof RangeError)
+    console.log(e.name);
+}
+//RangeError
+
