2255f30a11510deb10bcde58a654a8b64da490d8
Gloria Chen <cmxgloria@gmail.com>
2019-08-06 10:23:59 +1000

    updated string object

--- string-object.js
+++ string-object.js
@@ -30,4 +30,25 @@ console.log(value.slice(5));
 //String
 
 
+var value = 'Some String';
+console.log(value.slice(5, 8));
+//Str, first argument include , not the second argument
+
+var value = 'Some String';
+console.log(value.slice(-4));
+//ring, last 4 charactors
+
+var value = 'Some String';
+console.log(value.split(' '));
+//(2) ["Some" ,"String"]
+
+var value = 'Some String';
+console.log(value.substr(0, 4));
+//Some, start with index 0, the first 4 charactors
+
+var value = 'Some String';
+console.log(value.substring(5, 6));
+//S,start with the first argument include, end with last argument not included
+
+
 
