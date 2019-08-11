921c0d5d4564175eb3b38d01063a4bdb6dd80c62
Gloria Chen <cmxgloria@gmail.com>
2019-08-11 12:29:22 +1000

    updated AMD format

--- .vscode/module/module.js
+++ .vscode/module/module.js
@@ -8,5 +8,21 @@ var scoreboard = function () {
   }
 }();
 
-//module format: Asynchronous Module Defition(AMD), CommonJS, Universal Module Defition(UMD)
+//module format: Asynchronous Module Defition(AMD), CommonJS, Universal Module Defition(UMD),SystemRegister, ES2015
+
+//module loaders: RequireJS(AMD), SystemJS(AMD, CommonJS, UMD, SystemRegister)
+
+
+//--AMD format
+//game.js
+define(['./player'], function (player) {
+  console.log('Starting game for' + player.getName());
+  function calculateScore() {
+    //calculate the score here
+  }
+  return
+  {
+    calculateScore: calculateScore
+  };
+});
 
