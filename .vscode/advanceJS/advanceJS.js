46b23b4478621080bda692cd76e14e75a8bf8b1f
Gloria Chen <cmxgloria@gmail.com>
2019-08-16 20:06:19 +1000

    binding confusion

--- .vscode/advanceJS/advanceJS.js
+++ .vscode/advanceJS/advanceJS.js
@@ -166,3 +166,33 @@ o3.fo(); //'bar3'
 //'bar==1'
 //'bar---2'
 //'bar--3'
+
+//another sample same method
+var o1 = {
+  bar: 'bar1',
+  foo: function () {
+    console.log(this.bar);
+  }
+};
+var o2 = { bar: 'bar2', foo: o1.foo };
+var bar = 'bar3';
+var foo = o1.foo;
+o1.foo();
+o2.foo();
+foo();
+//'bar1'
+//'bar2'
+//'bar3'
+
+function foo() {
+  var bar = 'bar1';
+  this.baz = baz;
+  this.baz();
+}
+function baz() {
+  console.log(this.bar);
+}
+var bar = 'bar2';
+foo();
+//'bar2
+
