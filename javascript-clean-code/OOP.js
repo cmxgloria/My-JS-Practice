55a5650d99033feae0549c37ac218760b11a9e5d
Gloria Chen <cmxgloria@gmail.com>
2019-08-26 17:07:01 +1000

    write with class

--- javascript-clean-code/OOP.js
+++ javascript-clean-code/OOP.js
@@ -20,3 +20,27 @@ basket.addProduct(3, water);
 basket.printShoppingInfo();
 // same output, this with OOP
 
+//if write the class look like as below
+function Product(_name, _price) {
+  const name = _name;
+  const price = _price;
+  this.getName = function () {
+    return name;
+  };
+  this.getPrice = function () {
+    return price;
+  };
+}
+function Basket() {
+  const products = [];
+  this.addProduct = function (amount, product) {
+    products.push(...Array(amount).fill(product));
+  };
+  this.calcTotal = function () {
+    return products
+      .map(product => product.getPrice())
+      .reduce((a, b) => a + b, 0);
+  };
+  this.printShoppingInfo = function () {
+    console.log('one has to pay in total: ' + this.calcTotal());
+  };
