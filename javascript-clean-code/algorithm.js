e0f18da44363dd4b2813f9285f138ed245305b46
Gloria Chen <cmxgloria@gmail.com>
2019-08-27 13:12:21 +1000

    find vowels sample

--- javascript-clean-code/algorithm.js
+++ javascript-clean-code/algorithm.js
@@ -24,3 +24,15 @@ const palindrome = str => {
   return str === str.split('').reverse().join('')
 }
 
+
+// find vowels sample
+const findVowels = str => {
+  let count = 0;
+  const vowels = ['a', 'e', 'i', 'o', 'u']
+  for (let char of str.toLowerCase()) {
+    if (vowels.includes(char)) {
+      count++
+    }
+  }
+  return count;
+}
