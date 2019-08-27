935d130d783d09a473d9cd5aa8ec80ff718d7be3
Gloria Chen <cmxgloria@gmail.com>
2019-08-27 12:22:41 +1000

    updated another sample

--- javascript-clean-code/algorithm.js
+++ javascript-clean-code/algorithm.js
@@ -14,3 +14,13 @@ const fizzbuzz = num => {
     }
   }
 }
+
+//another sample
+const palindrome = str => {
+  // turn the string to lowercase
+  str = str.toLowerCase()
+  // reverse input string and return the result of the
+  // comparisong
+  return str === str.split('').reverse().join('')
+}
+
