2c226c8504a333b437a84ed0f8419e79fbc30a16
Gloria Chen <cmxgloria@gmail.com>
2019-08-31 20:51:39 +1000

    updated

--- .vscode/advanceJS/advanceJS.js
+++ .vscode/advanceJS/advanceJS.js
@@ -534,3 +534,4 @@ b2.speak();
 //constructor is a function that is called with the new key in front of it.
 //prototype is a linkage from one object to antoher object . it comes from two ways: it comes from the object create or we can get it indirectly as step two of four steps of the new keyword
 //how to prototype affect an object? if they cant handle object or property, that the property or method reference, it delegate up to prototype chain.
+//how to find out the object prototype points to? --Dunder proto, object, getprototypeof
