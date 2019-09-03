33e7e9a66e5557bf7f6c73bbf30341414035278a
Gloria Chen <cmxgloria@gmail.com>
2019-09-03 16:24:31 +1000

    get rid of protptype

--- .vscode/advanceJS/advanceJS.js
+++ .vscode/advanceJS/advanceJS.js
@@ -609,3 +609,20 @@ Bar.speak = function () {
 var b1 = Object.create(Bar);
 b1.init("b1");
 b1.speak();
+
+// try to get rid of the prototype with the same result
+var Foo = {
+  init: function (who) {
+    this.me = who;
+  },
+  identify: function () {
+    return " I am" + this.me;
+  }
+};
+var Bar = Object.create(Foo);
+Bar.speak = function () {
+  alert("Hello, " + this.identify + ".");
+};
+var b1 = Object.create(Bar);
+b1.init("b1");
+b1.speak();
