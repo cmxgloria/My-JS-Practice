f885b891bca76ad61003bac483296382f5d268a2
Gloria Chen <cmxgloria@gmail.com>
2019-09-03 13:36:22 +1000

    added similar sample

--- .vscode/advanceJS/advanceJS.js
+++ .vscode/advanceJS/advanceJS.js
@@ -573,3 +573,22 @@ Bar.prototype.speak = function () {
 };
 var b1 = new Bar("b1");
 b1.speak();
+
+//this coding is same as 
+function Foo(who) {
+  this.me = who;
+}
+Foo.prototype.identify = function () {
+  return "I am " + this.me;
+};
+function Bar(who) {
+  Foo.call(this, who);
+}
+Bar.prototype = Object.create(Foo.prototype);
+Bar.prototype.speak = function () {
+  alert("Hello, " + this.identify + ".");
+};
+var b1 = Object.create(Bar.prototype);
+Bar.call(b1, "b1");
+b1.speak();
+
