d997cfc7b02e58a18b081a8207c816ce9bfbf14b
Gloria Chen <cmxgloria@gmail.com>
2019-09-05 12:56:12 +1000

    Async pattern: separate callbacks

--- .vscode/advanceJS/advanceJS.js
+++ .vscode/advanceJS/advanceJS.js
@@ -806,3 +806,20 @@ dogA.dogName;
 dogA.dogName = "mandia";
 dogA.dogName;
 
+
+//Async pattern: separate callbacks
+function trySomething(ok, err) {
+  setTimeout(function () {
+    var num = Math.random();
+    if (num > 0.5) ok(num);
+    else err(num);
+  }, 1000);
+}
+trySomething(
+  function (num) {
+    console.log("Success: " + num);
+  },
+  function (num) {
+    console.log("sorry: " + num);
+  }
+);
