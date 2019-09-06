258df5fae3926fa05f9667ed34607d31375d01e2
Gloria Chen <cmxgloria@gmail.com>
2019-09-06 12:07:46 +1000

    updated

--- .vscode/advanceJS/advanceJS.js
+++ .vscode/advanceJS/advanceJS.js
@@ -845,3 +845,12 @@ getData(10, function (num1) {
 });
 
 
+//Async Pattern: JQuery-style promises
+var wait = jQuery.Deferred();
+var p = wait.promise();
+p.done(function (value) {
+  console.log(value);
+});
+setTimeout(function () {
+  wait.resolve(Math.random())
+}, 1000);
