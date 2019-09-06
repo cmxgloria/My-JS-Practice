f7f46b3f49d83be7ddef25cbc4b746adfc37315c
Gloria Chen <cmxgloria@gmail.com>
2019-09-06 13:07:22 +1000

    sequence tasks

--- .vscode/advanceJS/advanceJS.js
+++ .vscode/advanceJS/advanceJS.js
@@ -914,3 +914,26 @@ ASQ()
   .then(function () {
     console.log("5 seconds passed!");
   });
+
+//sequence tasks
+function getData(d) {
+  return ASQ(function (done) {
+    setTimeout(function () {
+      done(d);
+    }, 2000)
+  });
+}
+ASQ()
+  .waterfall(
+    function (done) { getData(10).pipe(done); },
+    function (done) { getData(30).pipe(done); }
+  )
+  .seq(function (num1, num2) {
+    var x = 1 + num1;
+    var y = 1 + num2;
+    return getData("meaning of life: " + (x + y));
+  })
+  .val(function (answer) {
+    console.log(answer);
+  });
+
