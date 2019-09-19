69c56f630e3808dbfec5b9755a8ca75515d402ff
Gloria Chen <cmxgloria@gmail.com>
2019-09-19 18:58:17 +1000

    even number n setTimeout

---
+++ .vscode/typical/typical.js
@@ -0,0 +1,27 @@
+//find out even number
+const findEvenNum = (arr) => {
+  const evenNum = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      evenNum.push(arr[i]);
+    }
+  }
+  return evenNum;
+}
+const testArr = [1, 4, 6, 8, 7];
+console.log(findEvenNum(testArr));
+
+//setTimeout sample
+console.log('a');
+let timmy = setTimeout(function () {
+  console.log('b');
+}, 1);
+let roxs = setTimeout(function () {
+  console.log('c');
+}, 10);
+let mat = setTimeout(function () {
+  console.log('d');
+}, 0);
+console.log('e');
+
+//async function
