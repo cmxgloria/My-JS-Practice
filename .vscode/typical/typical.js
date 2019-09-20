3746b781855ff705e82313b4a727fe3079f05bd8
Gloria Chen <cmxgloria@gmail.com>
2019-09-20 20:50:48 +1000

    JS questions

--- .vscode/typical/typical.js
+++ .vscode/typical/typical.js
@@ -68,3 +68,12 @@ sayHelloToSarah();
 //'Hello Sarah'
 
 //closure
+
+
+//JS questions
+//what is your most important tools for you and why?-It depends on the application(give examples of different too suit the application)
+//what is your favourite feature in Node and why?--LTS support(async await, generators ES 6 support)
+//what is your favourite feature in css and why?--CSS grid and flexbox
+//what is your favourite feature in Javascript and why?--Object.freeze,fetch,guading(async/await,generator,ES6 support,map/filter/reduce)
+//what is the most difficult part of a frontend project and why?--To keep up the test coverage, but on the right tools, keep animation at 60 fps, avoid tech debt(make everything consistent in every browser)
+//how would you fix a slow browser?--use the browser profiler to investigate, reduce the number of http call,use vanilla javascript(ship less code and improve the load time,reduce asset weight)
